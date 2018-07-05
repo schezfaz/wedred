c85015142c17210c88e355e0c6f08cdf2f99e42c
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2018-07-05 15:19:18 +0530

    pagination ui (temp) added
    functionality is left

--- wedred/mainPage/home.js
+++ wedred/mainPage/home.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $(window).bind('scroll', function() {
     var navHeight = $( window ).height() - 70;
           if ($(window).scrollTop() > navHeight) {
-              $('nav').addClass('fixed');
+              $('.navigationBar').addClass('fixed');
           }
           else {
-              $('nav').removeClass('fixed');
+              $('.navigationBar').removeClass('fixed');
           }
      });
  });
