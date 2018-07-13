9df60a2db92c0a67cec22eb78bbc1d4c02586f39
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2018-07-13 15:11:28 +0530

    added parallax and image gallery

---
+++ wedred/mainPage/navbar.php
@@ -0,0 +1,43 @@
+</<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        nav {
+            position: absolute;
+            top: 0;
+            width: 100%;
+            height: 70px;
+            font-weight: bolder;
+            text-align: center;
+            font-size: 20px;
+            color: white;
+            background: rgba(0,0,0,0.5);
+        }
+        nav li {
+            display: inline-block;
+            padding: 24px 10px;
+        }
+        nav li a {
+            color: #757575;
+            text-transform: uppercase;
+        }
+        nav li a:hover{
+            color:rgba(252, 106, 106);
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navigationBar" id="navigationBarr" data-spy="affix">
+        <ul>
+        <!--<li><a href="#">Home</a></li> -->
+            <li><a href="#abt">About</a></li>
+            <!-- <li><a href="#">Craft</a></li> -->
+            <li><a href="#film">Film<!-- Services --></a></li>
+            <li><a href="#tra">Travel</a></li>
+            <!-- <li><a href="#">Team</a></li> -->
+            <li><a href="#con">Contact</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
